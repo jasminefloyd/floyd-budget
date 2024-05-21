57b88f9fbbe562d27dfef73ee272d3c717a93889
jasminefloyd <42697356+jasminefloyd@users.noreply.github.com>
2024-05-21 10:00:03 -0500

    refactor

--- actions.js
+++ actions.js
@@ -2,11 +2,17 @@
 const budget = {
     id: 0,
     name: "Test Budget",
-    income: [],
+    salary: [],
+    salaryTypeU: [],
     expense: [],
-    totalIncome: 5000,
-    totalExpense: 4500,
-    moneyLeft: 0
+    expenseTypeU: [],
+    expenseTypes: ["Rent" , "Transportation", "Food", "Utilities", "Entertainment", "Misc", "Other"],
+    sum: 0,
+    moneyLeft: 0,
+    totalIncome: 0,
+    totalExpense: 0
+
+
 }
 
 //DOM Elements
@@ -16,6 +22,16 @@ const homeBtnEl = document.getElementById('home-btn')
 const budgetDetailsPageHeaderTitleEl = document.getElementById('budget-details-page-header-title')
 const newIncomeBtnEl = document.getElementById('new-income-btn')
 const newExpenseBtnEl = document.getElementById('new-expense-btn')
+budgetDetailsPageHeaderTitleEl.innerHTML = `${budget.name}`
+
+const typeSelectionEl = document.getElementById("selectType")
+const newExpenseDropEl = document.getElementById('droppy')
+const expenseInputAmountEl = document.getElementById('expense-input-amount')
+const incomeInputAmountEl = document.getElementById('income-input-amount')
+const submitExpenseBtnEl = document.getElementById('submit-expense')
+const submitIncomeBtnEl = document.getElementById('submit-income')
+
+
 
 
 //top section
@@ -24,11 +40,7 @@ const moneyLeftAmtEl = document.getElementById('money-left-amt')
 const totalIncomeAmtEl = document.getElementById('total-income-amt')
 const progressBarEl = document.querySelector('progress')
 
-budget.moneyLeft = budget.totalIncome - budget.totalExpense
 
-budgetDetailsPageHeaderTitleEl.innerHTML = `${budget.name}`
-moneyLeftAmtEl.innerHTML = `$${budget.moneyLeft}`
-totalIncomeAmtEl.innerHTML = `$${budget.totalIncome}`
 progressBarEl.setAttribute('max', budget.totalIncome)
 progressBarEl.setAttribute('value', budget.totalExpense)
 
@@ -36,10 +48,18 @@ progressBarEl.setAttribute('value', budget.totalExpense)
 
 
 //middle section
+const tabLinks = document.querySelectorAll(".tab-link")
+const tabContents = document.querySelectorAll(".tab-list")
+
+
+
+
 //bottom section
-const incomeInputEl = document.getElementById("income-type")
-const incomeAmountEl = document.getElementById("income-input-amount")
-const inputAmountEl = document.getElementById("input-amount")
+const incomeInputEl = document.getElementById('income-type')
+const incomeAmountEl = document.getElementById('income-input-amount')
+const inputAmountEl = document.getElementById('input-amount')
+const incomeListEl = document.getElementById('salary')
+const expenseListEl = document.getElementById('expense')
 
 
 
@@ -47,27 +67,61 @@ const inputAmountEl = document.getElementById("input-amount")
 
 //Functions
 
-//Listeners 
-newExpenseBtnEl.addEventListener("click", function() {
-    const expenseType = typeSelectionEl.value
-    const expenseAmount = Number(inputAmountEl.value)
 
-    budget.expense.push(expenseAmount)
-    budget.expenseTypeU.push(expenseType) 
-    document.getElementById("create-expense").style.display = "none";
-    document.getElementById("income-tab").classList.remove("active")
-    document.getElementById("expense-tab").classList.add("active")
-    renderBudget()
-})
 
-newIncomeBtnEl.addEventListener("click", function() {
-    const incomeAmount = Number(totalIncomeAmtEl.value)
-    const incomeType = incomeInputEl.value
-    budget.salary.push(incomeAmount)
-    budget.salaryTypeU.push(incomeType)
-    document.getElementById("create-income").style.display = "none";
-    renderSalary()
-})
+
+function calculateExpense() {
+    const sum = budget.expense.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    budget.totalExpense = sum
+    budget.moneyLeft = budget.totalIncome - budget.totalExpense
+    moneyLeftAmtEl.innerHTML = `$${budget.moneyLeft}`
+
+}
+
+function renderExpense() {
+    let expenseListItems = " " 
+    for (let i = 0; i < budget.expense.length; i++) {
+        expenseListItems += `<li>${budget.expenseTypeU[i]} - $${budget.expense[i]}</li><hr />` 
+    }
+    expenseListEl.innerHTML = expenseListItems
+    calculateIncome()
+    calculateExpense()
+    clearField()
+}
+
+
+
+
+function calculateIncome() {
+    const sum = budget.salary.reduce((accumulator, currentValue) => accumulator + currentValue, 0)
+    budget.totalIncome = sum
+    totalIncomeAmtEl.innerHTML = `$${budget.totalIncome}`
+}
+
+
+function renderIncome() {
+    let salaryListItems = "" 
+    for (let i = 0; i < budget.salary.length; i++) {
+        salaryListItems += `<li>${budget.salaryTypeU[i]} - $${budget.salary[i]}</li><hr />` 
+    }
+    incomeListEl.innerHTML = salaryListItems 
+    calculateIncome()
+    calculateExpense()
+    clearField()
+}
+
+
+
+
+function clearField() {
+    document.getElementById("selectType").value = ""
+    document.getElementById("expense-input-amount").value = "$0"
+    document.getElementById("income-type").value = ""
+    document.getElementById("income-input-amount").value = ""
+    
+
+}
+
 
 
 
@@ -85,12 +139,84 @@ function closePopup() {
     document.getElementById("create-expense").style.display = "none";
 }
 
+//Listeners 
+newExpenseBtnEl.addEventListener("click", function() {
+    document.getElementById("create-expense").style.display = "none";
+    document.getElementById("income-tab").classList.remove("active")
+    document.getElementById("expense-tab").classList.add("active")
+
+    for (let i = 0; i < budget.expenseTypes.length; i++) {
+        let option = document.createElement("option")
+        option.value = budget.expenseTypes[i]
+        option.text = budget.expenseTypes[i]
+        typeSelectionEl.appendChild(option)
+    }
+
+    submitExpenseBtnEl.addEventListener('click', function() {
+        const expenseType = typeSelectionEl.value
+        const expenseAmount = Number(expenseInputAmountEl.value)
+    
+        budget.expense.push(expenseAmount)
+        budget.expenseTypeU.push(expenseType) 
+
+        closePopup()
+        renderExpense()
+
+        // 
+
+    })
+
+   
+})
+
+newIncomeBtnEl.addEventListener("click", function() {
+    document.getElementById("create-expense").style.display = "none";
+    document.getElementById("income-tab").classList.add("active")
+    document.getElementById("expense-tab").classList.remove("active")
+
+    submitIncomeBtnEl.addEventListener('click', function() {
+        const incomeAmount = Number(incomeInputAmountEl.value)
+        const incomeType = incomeInputEl.value
+
+        budget.salary.push(incomeAmount)
+        budget.salaryTypeU.push(incomeType)
+
+        closePopup()
+        renderIncome()
+
+    })
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.getElementById('new-expense-btn').addEventListener('click', openExpensePopup);
     document.getElementById('new-income-btn').addEventListener('click', openIncomePopup);
     document.getElementById('close-income-popup').addEventListener('click', closePopup);
     document.getElementById('close-expense-popup').addEventListener('click', closePopup);
+    tabLinks.forEach((link) => {
+        link.addEventListener("click", (event) => {
+        // Deactivate all tabs
+        tabLinks.forEach((link) => link.classList.remove("active"))
+        tabContents.forEach((content) => content.classList.remove("active"))
+    
+        // Activate the clicked tab
+        const tab = event.currentTarget.dataset.tab
+        event.currentTarget.classList.add("active")
+        document.getElementById(tab).classList.add("active")
+        })
+    })
 });
 
 
